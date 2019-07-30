84e666bd538e416a4fbcd4f41933acc1204e4508
raphaeldoukhan <52462993+raphaeldoukhan@users.noreply.github.com>
2019-07-30 07:52:42 +0200

    Update controller.rb

--- lib/controller.rb
+++ lib/controller.rb
@@ -13,19 +13,11 @@ class ApplicationController < Sinatra::Base
 
     post '/gossips/new/' do
       Gossip.new("#{params["gossip_author"]}", "#{params["gossip_content"]}").save
-    end
-
-    post '/gossips/new/' do
-      puts "Salut, je suis dans le serveur"
-      puts "Ceci est le contenu du hash params : #{params}"
-      puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
-      puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
-      puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
       redirect '/'
     end
 
-    get '/gossips/:index/' do
-     Gossip.find(params['index'].to_i)
+    get '/gossips/:id' do
+      erb :gossip, locals: {gossip:Gossip.find(params['id'].to_i), id:params['id'], comments:Comment.all(params['id'].to_i) }
     end
 
 end
